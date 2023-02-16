d040983b7bd9203098e35585c35d4f1a21d0c259
vedikapatgaonkar33 <vedikapatgaonkar33@gmail.com>
2023-02-16 10:39:34 +0530

    first variable assignment
    first variable assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,25 @@
+console.log("********* step 1 ********* ");
+var myFullName;
+console.log(myFullName);
+myFullName="Vedika Patgaonkar"
+console.log( " Full Name :" ,myFullName);
+myFullName="Vedika Pradeep Patgaonkar"
+console.log( " Full Name :",myFullName);
+console.log("********* step 2 ********* ");
+var myLove="I Love Only JS ";
+console.log(myLove);
+console.log("********* step 3 ********* ");
+var firstName="Vedika";
+var lastName="Patgaonkar";
+var age="23"
+var address="pune"
+var pincode="411038"
+var gender="Female"
+var isMarried="No"
+console.log( "First Name: ",firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Address:",address);
+console.log("PinCode:",pincode);
+console.log("Gender:",gender);
+console.log("Is Married:",isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,23 @@
 console.log(" Vedika patgaonkar ");
+console.log(" 31 ");
+console.log(" pune ");
+var fullName="vedika patgaonkar";
+console.log(fullName);
+console.log(fullName);
+console.log(fullName);
+var city;
+console.log(city);
+city="pune"
+console.log(city);
+city="surat"
+console.log(city);
+
+var age="31";//variable declaration and initialization
+age=32;
+age=33;
+console.log(age);
+
+var university;// variable declaration
+university="SGBAU";//variable initailization
+console.log(university);
+
