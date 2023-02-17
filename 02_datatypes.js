5571fc817ea042596e57e5f0341420ad6b3dbfc4
vedikapatgaonkar33 <vedikapatgaonkar33@gmail.com>
2023-02-17 15:37:56 +0530

    datatypes
    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,32 @@
+var myName="vedika patgaonkar"; //string
+var fatherName="pradeep patgaonkar"
+var age=23;
+var weight=75; // integer
+
+var isMarried=true; //boolen
+var isLazy=false;
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+console.log("typeOf variabe age :",typeof age);
+console.log("typeOf variabe weight :",typeof weight);
+console.log("typeOf variabe isMarried :",typeof isMarried);
+console.log("typeOf variabe isLazy :",typeof isLazy);
+
+var graduation;
+console.log("value of this varible graduation :",graduation);
+console.log("typeOf variabe graduation :",typeof graduation);
+
+var city=null;
+console.log("value of this varible city :",city);
+console.log("typeOf variabe city :",typeof city);
+
+
+console.log("swapping the variables");
+var num1=100; //200
+var num2=200; //100
+var temp=num1;
+num1=num2;
+num2=temp;
+console.log(num1,num2);
